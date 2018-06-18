07e779f529164870a048028215332a2985f5bd80
boying.shi <shi.boying@gmail.com>
2018-06-17 21:03:34 -0700

    bs - add api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'images#index'
   resources :images, only: %i[create show new]
   resources :feedbacks, only: [:new]
+
+  namespace :api do
+    resource :feedbacks, only: [:create]
+  end
 end
