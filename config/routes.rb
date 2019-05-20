3799d4eb437ae4fb0d93c971d40188a9eede23c6
Vikram Gopali <vikram.gopali@appfolio.com>
2019-05-20 16:45:17 -0700

    vg - set up index page with images displayed in newest first order

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :images, only: %i[create show new]
-  root 'images#new'
+  root 'images#index'
 end
