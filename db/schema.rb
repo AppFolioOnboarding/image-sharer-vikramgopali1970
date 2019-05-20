dae2383c16b1fae996cca3fe45c106d0911ceae5
Vikram Gopali <vikram.gopali@appfolio.com>
2019-05-20 11:25:34 -0700

    vg - added model to store the url of images

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_17_225226) do
 
+  create_table "images", force: :cascade do |t|
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
