0661f2a1226e0c5f61abeacbcb85e4e79bfa4eb0
Vikram Gopali <vikram.gopali@appfolio.com>
2019-05-22 14:52:02 -0700

    vg - added navbar on top and a sticky footer across the application

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -5,4 +5,16 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  def test__navbar_present_at_home_page
+    get root_url
+    assert_response :ok
+    assert_select '#navigation-header'
+  end
+
+  def test__footer_present_at_home_page
+    get root_path
+    assert_response :ok
+    assert_select '#sticky-footer'
+  end
 end
