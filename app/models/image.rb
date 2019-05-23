af0dbb6ef8869355f925d1b23754f9dabc4cae88
Vikram Gopali <vikram.gopali@appfolio.com>
2019-05-22 18:27:37 -0700

    changes to the models to accept the tags

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ApplicationRecord
+  acts_as_taggable
   validates :image_url, format: { with: URI.regexp(%w[http https]) }
   validates :image_url, presence: true
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
   def setup
-    @img_obj = Image.new(image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTf3RyZZVjGm23hkH1Lp1xndGGSkv6OlCkRtixg_f3siyp9UAY')
+    @img_obj = Image.new(image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTf3RyZZVjGm23hkH1Lp1xndGG
+Skv6OlCkRtixg_f3siyp9UAY', tag_list: 'office, work')
   end
 
   def test_valid_url
@@ -19,5 +20,17 @@ class ImageTest < ActiveSupport::TestCase
     @img_obj.image_url = ''
     assert_not @img_obj.valid?, 'Image url cannot be blank'
     assert_equal ['is invalid', "can't be blank"], @img_obj.errors.messages[:image_url]
+  end
+
+  def test_tag_list
+    assert @img_obj.valid?
+    assert_equal @img_obj.tag_list, %w[office work]
+  end
+
+  def test_tag_list_empty
+    @img_obj = Image.new(image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTf3RyZZVjGm23hkH1Lp
+1xndGGSkv6OlCkRtixg_f3siyp9UAY')
+    assert @img_obj.valid?
+    assert_empty @img_obj.tag_list
   end
 end
