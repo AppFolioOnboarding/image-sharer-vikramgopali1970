f9ee15d61fc857c1d36f913dba11e50367be01c1
ke zhang <kim.zhang@appfolio.com>
2019-05-28 15:54:56 -0700

    Revert "Image sharing react setup vikramgopali issue#1"

--- app/javascript/image_sharing/stores/FeedbackStore.js
+++
@@ -1,5 +0,0 @@
-export class FeedbackStore {
-  /* Implement your feedback store*/
-}
-
-export default FeedbackStore;
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'images#index'
   resources :images, only: %i[create show new]
-  resources :feedbacks, only: [:new]
-
-  namespace :api do
-    resource :feedbacks, only: [:create]
-  end
+  root 'images#index'
 end
