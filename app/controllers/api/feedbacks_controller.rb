e8136981f863fb1fd4f481c4cfaeb87542d5df39
Vikram Gopali <vikram.gopali@appfolio.com>
2019-05-29 17:33:40 -0700

    vg - updated the create endpoint to handle the post request for feedback

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,5 +1,19 @@
 module Api
   class FeedbacksController < ApplicationController
-    def create; end
+    def create
+      name = feedback_params[:name]
+      comment = feedback_params[:comments]
+      if name.empty? || comment.empty?
+        render json: { data: 'Both fields required', type: 'danger' }, status: :ok
+      else
+        render json: { data: 'Feedback received. Thank you..!!', type: 'success' }, status: :ok
+      end
+    end
+
+    private
+
+    def feedback_params
+      params.require(:feedback).permit(:name, :comments)
+    end
   end
 end
